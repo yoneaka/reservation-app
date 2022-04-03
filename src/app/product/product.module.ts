a65fc502a5b981bcb8f26acd18b0dd606831448b
YoneAka <github>
2022-04-03 16:52:42 +0900

    商品一覧ページ作成

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product.component';
+
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: 'detail', component: ProductDetailComponent }
+    ]
+  }
+
+
+];
+
+@NgModule({
+  declarations: [
+    ProductDetailComponent,
+    ProductListComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
